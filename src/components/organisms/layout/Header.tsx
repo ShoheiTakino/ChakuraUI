541f9dcb88f37fe773ad9de809a8318d5f01f92d
ShoheiTakino <104742356+ShoheiTakino@users.noreply.github.com>
2022-07-12 01:00:59 +0900

    breakpointnav

--- src/components/organisms/layout/Header.tsx
+++ src/components/organisms/layout/Header.tsx
@@ -1,5 +1,6 @@
-import { Flex, Heading } from "@chakra-ui/react";
+import { Box, Flex, Heading } from "@chakra-ui/react";
 import { memo, VFC } from "react";
+import { Link } from "react-router-dom";
 
 export const Header: VFC = memo(() => {
   return (
@@ -14,6 +15,17 @@ export const Header: VFC = memo(() => {
       <Heading as="h1" fontSize={{ base: "md", md: "lg" }}>
         ユーザー管理アプリ
       </Heading>
+      <Flex
+        align="center"
+        fontSize="sm"
+        flexGrow={2}
+        display={{ base: "none", md: "flex" }}
+      >
+        <Box pr={4}>
+          <Link>ユーザー一覧</Link>
+        </Box>
+        <Link>設定</Link>
+      </Flex>
     </Flex>
   );
 });
